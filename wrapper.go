495f7ee451f92ad4f093c167dce4e51b1989beb6
dushaoshuai <1528606126@qq.com>
2024-02-08 00:49:09 +0800

    feat(wrapper): NewSession

--- wrapper.go
+++ wrapper.go
@@ -1,5 +1,17 @@
 package wrapper
 
+import (
+	"xorm.io/xorm"
+)
+
 type Session struct {
 	*session
+}
+
+func NewSession(embedded *xorm.Session) *Session {
+	return &Session{
+		session: &session{
+			Session: embedded,
+		},
+	}
 }
